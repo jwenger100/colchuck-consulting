6483bf73e4295890f48769ce3b59e3858e2acd66
kylejoe <halterman.kyle@gmail.com>
2024-02-24 16:22:04 -0800

    Possible fix for next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   output: "export",
   env: {
     NEXT_PUBLIC_BASE_PATH: "",
   },
 };
-
-module.exports = nextConfig;
